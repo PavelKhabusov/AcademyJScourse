24f3201edeac2143208fb2b9efb6f7394161c667
Pavel <xabusva20@gmail.com>
2019-03-04 15:05:41 +1000

    Update script.js

--- FirstLesson/js/script.js
+++ FirstLesson/js/script.js
@@ -12,59 +12,52 @@ let appData = {
 	savings: false
 };
 
-for (let i = 0; i < 2; i++) {
-	let outlay = prompt("Введите обязательную статью расходов в этом месяце", ''),
-			outlayCost = +prompt("Во сколько обойдется?", '');
-
-	if (typeof(outlay) === 'string' && typeof(outlay) != null && typeof(outlayCost) != null && 
-			outlay != '' && outlayCost != '' && outlay.length < 50 && outlay.length > 3) {
-		appData.expenses[outlay] = outlayCost;
-	} else {
-		i--;
-		alert("Данные введены неверно!", '');
+function chooseReqExpenses(num) {
+	for (let i = 0; i < num; i++) {
+		let outlay = prompt("Введите обязательную статью расходов в этом месяце", ''),
+				outlayCost = +prompt("Во сколько обойдется?", '');
+	
+		if (typeof(outlay) === 'string' && typeof(outlay) != null && typeof(outlayCost) != null && 
+				outlay != '' && outlayCost != '' && outlay.length < 50 && outlay.length > 3) {
+			appData.expenses[outlay] = outlayCost;
+		} else {
+			i--;
+			alert("Данные введены неверно!", '');
+		}
 	}
 }
 
-// let i = 2;
-// while(i) {
-// 	let outlay = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 			outlayCost = +prompt("Во сколько обойдется?", '');
-
-// 	if (typeof(outlay) === 'string' && typeof(outlay) != null && typeof(outlayCost) != null && 
-// 			outlay != '' && outlayCost != '' && outlay.length < 50 && outlay.length > 3) {
-// 		appData.expenses[outlay] = outlayCost;
-// 	} else {
-// 		i--;
-// 		alert("Данные введены неверно!", '');
-// 	}
-// 	i--;
-// }
-
-// let i = 2;
-// do {
-// 	let outlay = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 			outlayCost = +prompt("Во сколько обойдется?", '');
-
-// 	if (typeof(outlay) === 'string' && typeof(outlay) != null && typeof(outlayCost) != null && 
-// 			outlay != '' && outlayCost != '' && outlay.length < 50 && outlay.length > 3) {
-// 		appData.expenses[outlay] = outlayCost;
-// 	} else {
-// 		i--;
-// 		alert("Данные введены неверно!", '');
-// 	}
-// 	i--;
-// } while (i);
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ваш бюджет на день = " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-	console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-	console.log("Средний уровень достатка");
-} else {
-	console.log("Высокий уровень достатка");
+function chooseOptExpenses(num) {
+	for (let i = 0; i < num; i++) {
+		let outlay = prompt("Статья необязательных расходов?", '');
+	
+		if (typeof(outlay) === 'string' && typeof(outlay) != null && 
+				outlay != '' && outlay.length < 50 && outlay.length > 3) {
+			appData.optionalExpenses[i+1] = outlay;
+		} else {
+			i--;
+			alert("Данные введены неверно!", '');
+		}
+	}
 }
 
+function detectDayBudget() {
+	appData.moneyPerDay = appData.budget / 30;
+	alert("Ваш бюджет на день = " + appData.moneyPerDay);
+}
+
+function detectLevel() {
+	if (appData.moneyPerDay < 100) {
+		console.log("Минимальный уровень достатка");
+	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+		console.log("Средний уровень достатка");
+	} else {
+		console.log("Высокий уровень достатка");
+	}
+}
+
+chooseReqExpenses(2);
+chooseOptExpenses(3);
+detectLevel();
+detectDayBudget();
 console.log(appData);
