ecf5ca68bf400ae910e2d1ee9600248b17c46130
Pavel <xabusva20@gmail.com>
2019-03-15 19:30:46 +1000

    es6

--- Yoga/js/new.js
+++ Yoga/js/new.js
@@ -1,8 +1,24 @@
-let age = document.getElementById('age');
-function showUser(surname, name) {
-  alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+class Options {
+  constructor(heigth, width, bg, fontSize, textAlign) {
+    this.heigth = heigth;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.textAlign = textAlign;
+  }
+  create() {
+    let div = document.createElement('div');
+    div.classList.add('new');
+    div.style.cssText = `width: ${this.width}px;
+    height: ${this.heigth}px;
+    background: ${this.bg};
+    font-size: ${this.fontSize}px;
+    text-align: ${this.textAlign};`
+    return div;
+  }
 }
-showUser.apply(age, ['John', 'Smith']);
-age.addEventListener('change', function() {
-  showUser.apply(age, ['John', 'Smith']);
-});
+let block = new Options(100, 100, '#007', 20, 'center'),
+    main = document.querySelector('.main');
+main.appendChild(block.create());
+let hey = document.querySelector('.new');
+console.log(hey);
